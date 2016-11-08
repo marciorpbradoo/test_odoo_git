dd96daca06956d7734bd49667a351a24ff9a7deb
Marcio Ribeiro <marcio.ribeiro@bradootech.com>
2016-11-08 10:20:03 -0200

    modified:   odoo_start_script.sh

--- odoo_start_script.sh
+++ odoo_start_script.sh
@@ -1,4 +1,4 @@
-now_date=$(date '+%m-%d-%y')
+ow_date=$(date '+%m-%d-%y')
 cd odoo/odoo8/
 ./odoo.py -d diet_facts --logfile=odoolog/$now_date --addons-path=./addons,./customaddons -c openerp-server-test
 
